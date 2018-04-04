771aba393ecef01ee7766a2f7df2fb1a1df86771
sainatha798 <sainatha798@gmail.com>
2018-04-04 23:56:48 +0530

    basic calc done

--- cal.py
+++ cal.py
@@ -1,14 +1,15 @@
 from tkinter import *
-
+from math import *
+import sympy
 
 class Calc():
 	def __init__(self,master):
 		"""Constructor method"""
 		master.title('Calulator') 
 		master.geometry()
-		self.e = Entry(master)
-		self.e.grid(row=0,column=0,columnspan=9,pady=1)
-		self.e.focus_set() #Sets focus on the input text area
+		self.expression = Entry(master)
+		self.expression.grid(row=0,column=0,columnspan=9,pady=1)
+		self.expression.focus_set() #Sets focus on the input text area
 
 		self.div='÷'
 		#self.newdiv=self.div.decode('utf-8')
@@ -18,9 +19,9 @@ class Calc():
 		Button(master,text='AC',width=3,command=lambda:self.clearall()).grid(row=1, column=4)
 		Button(master,text='C',width=3,command=lambda:self.clear1()).grid(row=1, column=5)
 		Button(master,text="+",width=3,command=lambda:self.action('+')).grid(row=4, column=3)
-		Button(master,text="x",width=3,command=lambda:self.action('x')).grid(row=2, column=3)
+		Button(master,text="x",width=3,command=lambda:self.action('*')).grid(row=2, column=3)
 		Button(master,text="-",width=3,command=lambda:self.action('-')).grid(row=3, column=3)
-		Button(master,text="÷",width=3,command=lambda:self.action(self.newdiv)).grid(row=1, column=3) 
+		Button(master,text="/",width=3,command=lambda:self.action('/')).grid(row=1, column=3) 
 		Button(master,text="%",width=3,command=lambda:self.action('%')).grid(row=4, column=2)
 		Button(master,text="7",width=3,command=lambda:self.action('7')).grid(row=1, column=0)
 		Button(master,text="8",width=3,command=lambda:self.action(8)).grid(row=1, column=1)
@@ -36,23 +37,45 @@ class Calc():
 		Button(master,text="(",width=3,command=lambda:self.action('(')).grid(row=2, column=4)
 		Button(master,text=")",width=3,command=lambda:self.action(')')).grid(row=2, column=5)
 		Button(master,text="√",width=3,command=lambda:self.squareroot()).grid(row=3, column=4)
-		Button(master,text="x^n",width=3,command=lambda:self.exponent()).grid(row=3, column=5)
+		Button(master,text="x^n",width=3,command=lambda:self.action('^')).grid(row=3, column=5)
 
 
 	def equals(self):
-		return
+		try:
+			out = sympy.sympify(self.expression.get())
+			if out=='zoo':
+				raise Exception
+			self.expression.delete(0,END)
+			self.expression.insert(0,out)
+		except:
+			self.expression.delete(0,END)
+			self.expression.insert(0,"Invalid Input")
 
 	def clearall(self):
-		return
+		self.expression.delete(0,END)
+		#return
 	def clear1(self):
-		return
+		self.text = self.expression.get()[:-1]
+		self.expression.delete(0,END)
+		self.expression.insert(0,self.text)
+		#return
 	def action(self,arg):
-		return
+		self.expression.insert(END,arg)
+		#return
 	def squareroot(self):
-		return
-	def exponent(self):
-		return
-
+		try:
+			out = sqrt(sympy.sympify(self.expression.get()))
+			self.expression.delete(0,END)
+			self.expression.insert(0,out)
+		except:
+			self.expression.delete(o,END)
+			self.expression.insert(0,"Invalid Input")
+		
+	'''def exponent(self):
+		return'''
+	'''def process(self):
+		self.text = self.expression.get()
+		self.text.replace('')'''
 root = Tk()
 calc = Calc(root)
 root.mainloop()
